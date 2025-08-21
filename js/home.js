0b7a26116c1a5f1cc1ac34f5ce57cf3112d87c84
mahmudul <mahmudul1106@gmail.com>
2025-08-21 09:43:39 +0600

    third push

--- js/home.js
+++ js/home.js
@@ -1,9 +1,15 @@
-document.getElementById('btn-add-money').addEventListener('click', function(){
-    const addMoney = document.getElementById('amount-input').value;
-    const pin = document.getElementById('pin-input').value;
-    const finalAmount = document.getElementById('final-amount').innerText;
-    console.log(Number(finalAmount) + Number(addMoney))
-    if(pin === 1234){
-        finalAmount = Number(finalAmount)+Number(addMoney)
+document.getElementById('btn-add-money').addEventListener('click', function () {
+const pin = document.getElementById('pin-input').value;
+const addMoney = document.getElementById('amount-input').value;
+        
+        const finalAmount = document.getElementById('final-amount').innerText;
+    if (pin === '1234') {
+        
+        const finalAmountNew = Number(finalAmount) + Number(addMoney)
+        document.getElementById('final-amount').innerText = finalAmountNew;
+
+    }
+    else{
+        alert('Wrong pin')
     }
 })
