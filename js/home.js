1be423ca96a5ad814dc6c7bfbb5b2654d28be27b
mahmudul <mahmudul1106@gmail.com>
2025-08-21 07:19:01 +0600

    Second push

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-console.log('Home js added')
+document.getElementById('btn-add-money').addEventListener('click', function(){
+    const addMoney = document.getElementById('amount-input').value;
+    const pin = document.getElementById('pin-input').value;
+    const finalAmount = document.getElementById('final-amount').innerText;
+    console.log(Number(finalAmount) + Number(addMoney))
+    if(pin === 1234){
+        finalAmount = Number(finalAmount)+Number(addMoney)
+    }
+})
